01cd550e4dd325570fc5ad9fc1fd7f11c0de8a45
chandan9074 <chandan15-11611@diu.edu.bd>
2021-08-06 12:24:30 +0600

    Setup the the total project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// import components
+import Grocery from './components/grocery';
+
+function App() {
+  return (
+    <div className="App">
+      <Grocery/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/grocery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Grocery = () => {
+    return ( 
+        <div>
+            <h1>Grocery</h1>
+            <button>Buy product</button>
+        </div>
+     );
+}
+ 
+export default Grocery;
