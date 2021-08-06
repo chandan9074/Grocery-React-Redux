c0c41943486ccf5d81a820288559e92293f47bf4
chandan9074 <chandan15-11611@diu.edu.bd>
2021-08-06 13:52:48 +0600

    feat: Create counter

--- src/components/grocery.jsx
+++ src/components/grocery.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { buy_grocery } from '../redux/index'
 
-const Grocery = () => {
+const Grocery = (props) => {
     return ( 
         <div>
-            <h1>Grocery</h1>
-            <button>Buy product</button>
+            <h1>Number of Grocery - {props.numOfProducts}</h1>
+            <button onClick={props.buy_grocery}>Buy product</button>
         </div>
      );
 }
+
+
+const mapStateToProps = state =>{
+  return {
+    numOfProducts: state.numOfProducts
+  }
+}
+
+const mapDispatchToProps = dispatch =>{
+  return {
+    buy_grocery: () => dispatch(buy_grocery())
+  }
+}
  
-export default Grocery;
+export default connect(mapStateToProps, mapDispatchToProps)(Grocery);
---
+++ src/redux/index.js
@@ -0,0 +1,1 @@
+export { buy_grocery} from './grocery/groceryAction'
